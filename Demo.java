6fd9634fd7b06814acfcfdc08d61089d883bc9e0
AlexanderMinko <60578360+AlexanderMinko@users.noreply.github.com>
2020-02-02 19:14:12 +0200

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+package com.itbulls.learnit.Minko;
+
+import java.io.IOException;
+
+public class Demo {
+	public static void main(String[] args) throws IOException {
+		HW1.sum();
+		HW1.areaOfTringle();
+		HW1.circumFerence();
+	}
+
+}
